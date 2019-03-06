dc8fd7ba1d97242635bfdb5157ab43ee477c82d3
nestorojedag <nestorojedagonzalez@gmail.com>
2019-03-06 13:11:14 +0000

    añadida parte1 y parte2

---
+++ parte2/run.py
@@ -0,0 +1,17 @@
+# Search methods
+
+from parte1 import search
+
+ab = search.GPSProblem('A', 'B'
+                       , search.romania)
+print("Anchura:")
+print(search.breadth_first_graph_search(ab).path())
+print("Profundidad:")
+print(search.depth_first_graph_search(ab).path())
+print("Branch and bound:")
+print(search.Branch_and_Bound(ab).path())
+
+
+# Result:
+# [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
+# [<Node B>, <Node F>, <Node S>, <Node A>] : 211 + 99 + 140 = 450
